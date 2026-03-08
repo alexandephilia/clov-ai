820a38e293108fed02b83700a64760fcf4516928
0xnihilist <freemasonga1@gmail.com>
2026-03-08 17:06:21 +0700

    Update clov.rb

--- Formula/clov.rb
+++ Formula/clov.rb
@@ -6,30 +6,30 @@
 class Clov < Formula
   desc "High-performance CLI proxy to minimize LLM token consumption"
   homepage "https://github.com/alexandephilia/clov-ai"
-  version "0.29.1"
+  version "0.34.2"
   license "MIT"
 
   on_macos do
     on_intel do
       url "https://github.com/alexandephilia/clov-ai/releases/download/v#{version}/clov-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_MAC_INTEL"
+      sha256 "1fad5cd0a8aaec35b5552460f22439396b2156e085a75e9f9e19f5176614c497"
     end
 
     on_arm do
       url "https://github.com/alexandephilia/clov-ai/releases/download/v#{version}/clov-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_MAC_ARM"
+      sha256 "d4fa302f592c3339e6e5dfe9799623280d93993c13251d527f228f300c1787f7"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/alexandephilia/clov-ai/releases/download/v#{version}/clov-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_INTEL"
+      url "https://github.com/alexandephilia/clov-ai/releases/download/v#{version}/clov-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "923963917d69847947f4f23cad76d6606a0b10bdd48602d4913dd15b6d43e712"
     end
 
     on_arm do
       url "https://github.com/alexandephilia/clov-ai/releases/download/v#{version}/clov-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_ARM"
+      sha256 "d7def04758aeacf7cca42647a286836e4336c35b857ab051e24e9aff869a4615"
     end
   end
 
